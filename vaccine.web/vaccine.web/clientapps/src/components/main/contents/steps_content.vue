9b73101b6867901487092d658b67ca0864cf1298
Yea <xyzyea@gmail.com>
2021-04-28 17:52:25 +0800

    Steps 元件 & 範例

--- vaccine.web/vaccine.web/clientapps/src/components/main/contents/steps_content.vue
+++ vaccine.web/vaccine.web/clientapps/src/components/main/contents/steps_content.vue
@@ -5,52 +5,45 @@
         </template>
         <template v-slot:app-content>
             <div id="app">
-                <v-btn @click="gotoStep(3)">gotoStep</v-btn>
-                <com-steps start-step="1" :steps="steps">
-                    <template v-slot:step-content>
-                        <v-stepper-content step="1">
-                            <v-card class="mb-12"
-                                    color="grey lighten-1"
-                                    height="200px"></v-card>
+                <label>範例一</label>
+                <com-steps ref-key="one"
+                           :steps="steps"
+                           :alt-labels="false">
+                    <template v-slot:step-1>
+                        <v-card class="mb-12" color="grey lighten-1" height="100px">內文 1</v-card>
+                        <v-btn color="primary" @click="gotoStep('one',2)">Continue</v-btn>
+                        <v-btn text @click="stepsCancel('one')">Cancel</v-btn>
+                    </template>
+                    <template v-slot:step-2>
+                        <v-card class="mb-12" color="grey lighten-1" height="100px">內文 2</v-card>
+                        <v-btn color="primary" @click="gotoStep('one',3)">Continue</v-btn>
+                        <v-btn text @click="stepsCancel('one')">Cancel</v-btn>
+                    </template>
+                    <template v-slot:step-3>
+                        <v-card class="mb-12" color="grey lighten-1" height="100px">內文 3</v-card>
+                        <v-btn color="primary" @click="stepsFinish('one')">Finish</v-btn>
+                        <v-btn text @click="stepsCancel('one')">Cancel</v-btn>
+                    </template>
+                </com-steps>
 
-                            <v-btn color="primary"
-                                   @click="e1 = 2">
-                                Continue
-                            </v-btn>
-
-                            <v-btn text>
-                                Cancel
-                            </v-btn>
-                        </v-stepper-content>
-                        <v-stepper-content step="2">
-                            <v-card class="mb-12"
-                                    color="grey lighten-1"
-                                    height="200px"></v-card>
-
-                            <v-btn color="primary"
-                                   @click="e1 = 3">
-                                Continue
-                            </v-btn>
-
-                            <v-btn text>
-                                Cancel
-                            </v-btn>
-                        </v-stepper-content>
-
-                        <v-stepper-content step="3">
-                            <v-card class="mb-12"
-                                    color="grey lighten-1"
-                                    height="200px"></v-card>
-
-                            <v-btn color="primary"
-                                   @click="e1 = 1">
-                                Continue
-                            </v-btn>
-
-                            <v-btn text>
-                                Cancel
-                            </v-btn>
-                        </v-stepper-content>
+                <label>範例二</label>
+                <com-steps ref-key="two"
+                           :steps="steps"
+                           :alt-labels="true">
+                    <template v-slot:step-1>
+                        <v-card class="mb-12" color="grey lighten-1" height="100px">內文 1</v-card>
+                        <v-btn color="primary" @click="gotoStep('two',2)">Continue</v-btn>
+                        <v-btn text @click="stepsCancel('two')">Cancel</v-btn>
+                    </template>
+                    <template v-slot:step-2>
+                        <v-card class="mb-12" color="grey lighten-1" height="100px">內文 2</v-card>
+                        <v-btn color="primary" @click="gotoStep('two',3)">Continue</v-btn>
+                        <v-btn text @click="stepsCancel('two')">Cancel</v-btn>
+                    </template>
+                    <template v-slot:step-3>
+                        <v-card class="mb-12" color="grey lighten-1" height="100px">內文 3</v-card>
+                        <v-btn color="primary" @click="stepsFinish('two')">Finish</v-btn>
+                        <v-btn text @click="stepsCancel('two')">Cancel</v-btn>
                     </template>
                 </com-steps>
             </div>
@@ -69,17 +62,24 @@
                 { 'stepNum': 1, 'title': '步驟 1' },
                 { 'stepNum': 2, 'title': '步驟 2' },
                 { 'stepNum': 3, 'title': '步驟 3' }
-            ]
+            ], // stepNum: 標籤編號, title: 標籤文
         }),
         computed: {
         },
-        props: ["startStep"],
+        props: [],
         created: function () {
-
         },
         methods: {
-            gotoStep: function (stepNum) {
-                this.$bus.$emit('gotoStep', stepNum);
+            gotoStep: function (refKey, stepNum) {
+                this.$bus.$emit(`${refKey}_gotoStep`, stepNum);
+            },
+            stepsFinish: function (refKey) {
+                alert(refKey + ' finish');
+                this.gotoStep(refKey, 1);
+            },
+            stepsCancel: function (refKey) {
+                alert(refKey + ' cancel');
+                this.gotoStep(refKey,1);
             }
         },
         components: {
--- vaccine.web/vaccine.web/clientapps/src/components/steps.vue
+++ vaccine.web/vaccine.web/clientapps/src/components/steps.vue
@@ -1,73 +1,18 @@
 ﻿<template>
-    <v-stepper v-model="e1">
+    <v-stepper v-model="e1" :alt-labels="altLabels">
         <v-stepper-header>
             <template v-for="(step,index) in steps">
-                <v-stepper-step :complete="e1 > step.stepNum"
-                                :step="step.stepNum" :key="index">
+                <v-stepper-step :complete="e1 > step.stepNum" :step="step.stepNum" :key="index">
                     {{step.title}}
                 </v-stepper-step>
-
-                <v-divider v-if="index !== steps.length - 1"></v-divider>
+                <v-divider v-if="index !== steps.length - 1" :key="index"></v-divider>
             </template>
-
-                    <!--<v-stepper-step :complete="e1 > 2"
-                        step="2">
-            Name of step 2
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step step="3">
-            Name of step 3
-        </v-stepper-step>-->
-</v-stepper-header>
+        </v-stepper-header>
 
         <v-stepper-items>
-            <slot name="step-content"></slot>
-            <!--<v-stepper-content step="1">
-                <v-card class="mb-12"
-                        color="grey lighten-1"
-                        height="200px"></v-card>
-
-                <v-btn color="primary"
-                       @click="e1 = 2">
-                    Continue
-                </v-btn>
-
-                <v-btn text>
-                    Cancel
-                </v-btn>
-            </v-stepper-content>-->
-
-            <!--<v-stepper-content step="2">
-                <v-card class="mb-12"
-                        color="grey lighten-1"
-                        height="200px"></v-card>
-
-                <v-btn color="primary"
-                       @click="e1 = 3">
-                    Continue
-                </v-btn>
-
-                <v-btn text>
-                    Cancel
-                </v-btn>
+            <v-stepper-content :step="step.stepNum" v-for="(step,index) in steps" :key="index">
+                <slot :name="`step-${step.stepNum}`"></slot>
             </v-stepper-content>
-
-            <v-stepper-content step="3">
-                <v-card class="mb-12"
-                        color="grey lighten-1"
-                        height="200px"></v-card>
-
-                <v-btn color="primary"
-                       @click="e1 = 1">
-                    Continue
-                </v-btn>
-
-                <v-btn text>
-                    Cancel
-                </v-btn>
-            </v-stepper-content>-->
         </v-stepper-items>
     </v-stepper>
 </template>
@@ -75,25 +20,19 @@
 <script>
     export default {
         data: () => ({
-            e1: 1,
-            //steps: [
-            //    ['Step 1', 'Content 1'],
-            //    ['Step 2', 'Content 2'],
-            //    ['Step 3', 'Content 3']
-            //]
+            e1: 1
         }),
         computed: {
-
+            
         },
-        props: ["startStep", "steps"],
+        props: ["refKey","steps","altLabels"],
         created: function () {
             var comp = this;
-            comp.$bus.$on('gotoStep', function (stepNum) {
+            comp.$bus.$on(`${comp.refKey}_gotoStep`, function (stepNum) {
                 comp.e1 = stepNum;
             });
         },
         methods: {
-            
         },
         components: {
         }
