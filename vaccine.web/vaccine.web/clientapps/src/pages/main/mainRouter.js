c5dd99ab6e1329d8e211374f259320412d0be2ff
micchang <micchang@g.hkxf.org>
2021-04-30 14:45:53 +0800

    修正 router

--- vaccine.web/vaccine.web/clientapps/src/pages/main/mainRouter.js
+++ vaccine.web/vaccine.web/clientapps/src/pages/main/mainRouter.js
@@ -4,9 +4,11 @@ import content_main from 'components/main/main.vue'
 import content_dialog from 'components/main/contents/dialog_content.vue'
 import content_confirm from 'components/main/contents/confirm_content.vue'
 import content_loading from 'components/main/contents/loading_content.vue'
+import content_steps from 'components/main/contents/steps_content.vue'
 
 export default new VueRouter({
     routes: [
+        { name: 'steps', path: '/compoments/steps', component: content_steps },
         { name: 'confirm', path: '/compoments/confirm', component: content_confirm },
         { name: 'dialog', path: '/compoments/dialog', component: content_dialog },
         { name: 'loading', path: '/compoments/loading', component: content_loading },
