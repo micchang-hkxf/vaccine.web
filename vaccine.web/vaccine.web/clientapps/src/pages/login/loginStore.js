553225f6a13eeef1104829431635c1d96ee4f03e
Yea <xyzyea@gmail.com>
2021-05-13 18:01:10 +0800

    疫苗-動線-forget

--- vaccine.web/vaccine.web/clientapps/src/pages/login/loginStore.js
+++ vaccine.web/vaccine.web/clientapps/src/pages/login/loginStore.js
@@ -10,6 +10,7 @@ export default new Vuex.Store({
             return new Promise(function (resolve, reject) {
                 var result = { uid: account.uid, state: '', state1: '' };
                 try {
+                    // TODO: 帳密檢核 POST api/User/Login
                     var exist = state.loginFake.accounts.find(f => f.uid == account.uid);
 
                     if (!exist) {
@@ -36,10 +37,11 @@ export default new Vuex.Store({
                 }
             });
         },
-        checkVerificationCode: function ({ state }, account) {
+        checkVerificationCode: function ({ state }, account) { 
             return new Promise(function (resolve, reject) {
                 var result = { uid: account.uid, state: '', state1: '' };
                 try {
+                    // TODO: 驗證手機驗證碼 GET api/User/Login
                     var exist = state.loginFake.accounts.find(f => f.uid == account.uid);
 
                     if (!exist) {
@@ -71,6 +73,7 @@ export default new Vuex.Store({
             return new Promise(function (resolve, reject) {
                 var result = { uid: account.uid, state: '' };
                 try {
+                    // TODO: 修改密碼 PUT api/User/Login
                     var exist = state.loginFake.accounts.find(f => f.uid == account.uid);
                     
                     if (!exist) {
@@ -99,20 +102,117 @@ export default new Vuex.Store({
                     reject(result);
                 }
             });
+        },
+        checkForgetPdUid: function ({ state }, account) {
+            return new Promise(function (resolve, reject) {
+                var result = { uid: account.uid, state: '' };
+                try {
+                    // TODO: 身分驗證 GET api/User/Login/ForgetPd
+                    var exist = state.loginFake.accounts.find(f => f.uid == account.uid);
+
+                    if (!exist) {
+                        result.state = 'not found';
+                        resolve(result);
+                        return;
+                    }
+
+                    if (exist.state === 'deactivate') {
+                        result.state = exist.state;
+                        resolve(result);
+                        return;
+                    }
+
+                    if (exist.state === 'network abnormal') {
+                        result.state = exist.state;
+                        throw result;
+                    }
+
+                    result.state = 'pass';
+                    resolve(result);
+                } catch (e) {
+                    reject(result);
+                }
+            });
+        },
+        checkForgetPdVerificationCode: function ({ state }, account) {
+            return new Promise(function (resolve, reject) {
+                var result = { uid: account.uid, state: '' };
+                try {
+                    // TODO: 驗證手機驗證碼 POST api/User/Login/ForgetPd
+                    var exist = state.loginFake.accounts.find(f => f.uid == account.uid);
+
+                    if (!exist) {
+                        result.state = 'not found';
+                        resolve(result);
+                        return;
+                    }
+                    
+                    if (exist.verificationCode !== account.verificationCode) {
+                        result.state = 'invalid';
+                        resolve(result);
+                        return;
+                    }
+
+                    if (exist.state === 'network abnormal') {
+                        result.state = exist.state;
+                        throw result;
+                    }
+
+                    result.state = 'pass';
+                    resolve(result);
+                } catch (e) {
+                    reject(result);
+                }
+            });
+        },
+        modifyPw: function ({ state }, account) {
+            return new Promise(function (resolve, reject) {
+                var result = { uid: account.uid, state: '' };
+                try {
+                    // TODO: 修改密碼 PUT api/User/Login/ForgetPd
+                    var exist = state.loginFake.accounts.find(f => f.uid == account.uid);
+
+                    if (!exist) {
+                        result.state = 'not found';
+                        resolve(result);
+                        return;
+                    }
+
+                    if (exist.resetPwState !== 'pass') {
+                        result.state = exist.resetPwState;
+                        resolve(result);
+                        return;
+                    }
+
+                    if (exist.state === 'network abnormal') {
+                        result.state = exist.state;
+                        throw result;
+                    }
+
+                    console.log(account.uid);
+                    console.log(account.newUpd);
+                    console.log(account.verificationCode);
+
+                    result.state = exist.resetPwState;
+                    resolve(result);
+                } catch (e) {
+                    reject(result);
+                }
+            });
         }
     },
     state: {
         loginFake : {
             accounts: [
-                { uid: "yea"  , upd: '123', state: 'pass'              , state1: ''                           ,  verificationCode: '123456', resetPwState: ''      },
-                { uid: "yea01", upd: '123', state: 'no management area', state1: ''                           ,  verificationCode: '123456', resetPwState: ''      },
-                { uid: "yea02", upd: '123', state: 'not member'        , state1: ''                           ,  verificationCode: '123456', resetPwState: ''      },
+                { uid: "yea"  , upd: '123', state: 'pass'              , state1: ''                           ,  verificationCode: '123456', resetPwState: 'pass'  },
+                { uid: "yea01", upd: '123', state: 'no management area', state1: ''                           ,  verificationCode: '123456', resetPwState: 'pass'  },
+                { uid: "yea02", upd: '123', state: 'not member'        , state1: ''                           ,  verificationCode: '123456', resetPwState: 'pass'  },
                 { uid: "yea03", upd: '123', state: 'not yet enabled'   , state1: 'first login'                ,  verificationCode: '123456', resetPwState: 'pass'  },
                 { uid: "yea04", upd: '123', state: 'not yet enabled'   , state1: 'password is about to expire',  verificationCode: '123456', resetPwState: 'pass'  },
                 { uid: "yea05", upd: '123', state: 'not yet enabled'   , state1: 'password has expired'       ,  verificationCode: '123456', resetPwState: 'pass'  },
                 { uid: "yea06", upd: '123', state: 'not yet enabled'   , state1: 'password must change'       ,  verificationCode: '123456', resetPwState: 'error' },
-                { uid: "yea07", upd: '123', state: 'deactivate'        , state1: ''                           ,  verificationCode: '123456', resetPwState: ''      },
-                { uid: "yea08", upd: '123', state: 'network abnormal'  , state1: ''                           ,  verificationCode: '123456', resetPwState: ''      },
+                { uid: "yea07", upd: '123', state: 'deactivate'        , state1: ''                           ,  verificationCode: '123456', resetPwState: 'pass'  },
+                { uid: "yea08", upd: '123', state: 'network abnormal'  , state1: ''                           ,  verificationCode: '123456', resetPwState: 'pass'  },
             ]
         }
     },
