d5c5d5833dd3df2631520e7ea355842fc2a62c94
huwyang <huwyang@macisk.idv.tw>
2021-05-14 11:51:38 +0800

    簽入 稽核管理

---
+++ vaccine.web/vaccine.web/clientapps/src/components/admin/contents/audit_content.vue
@@ -0,0 +1,209 @@
+﻿<template>
+    <app-layout>
+        <template v-slot:navigation>
+            <app-menu></app-menu>
+        </template>
+        <template v-slot:content-title>
+            稽核
+        </template>
+        <template v-slot:app-content>
+            <div id="app">
+                <v-card style="margin-left: 20px; margin-right: 20px; margin-top: 20px;">
+                    <com-table ref-key="table" :headers="headers" :items="items" :total-count="totalCount"
+                               :items-per-page="itemsPerPage" :total-visible="totalVisible" :show-select="showSelect"
+                               :change-page="changePage"
+                               style="margin-left: 15px;padding-top: 15px;margin-right: 15px;">
+                        <template v-slot:item.quota>
+                            <div>45/<span style="color:dimgrey">60</span></div>
+                        </template>
+                        <template v-slot:search-bar>
+                            <div style="display:flex;justify-content:flex-start;margin-left:10px;margin-top:10px;">
+                                <v-row>
+                                    <v-col cols="7">
+                                        <v-select v-model="selectType"
+                                                  :items="getTypes"
+                                                  item-text="name"
+                                                  item-value="name"
+                                                  label="全部報表類型"
+                                                  :menu-props="{ bottom: true, offsetY: true }"
+                                                  outlined
+                                                  dense
+                                                  clearable
+                                                  style="margin-right: 10px;"
+                                                  class="search-filter"
+                                                  return-object>
+                                        </v-select>
+                                    </v-col>
+                                    <v-col cols="7">
+                                        <v-text-field v-model="keyWord" label="使用者/所屬單位/檔名關鍵字" outlined dense clearable></v-text-field>
+                                    </v-col>
+                                    <v-btn icon color="#626781" style="top:5px;" :ripple="false"
+                                           @click="getAudit(1)">
+                                        <v-icon>fas fa-search</v-icon>
+                                    </v-btn>
+                                </v-row>
+                            </div>
+
+
+                        </template>
+
+                        <template v-slot:toolbar-action>
+                          <span> 檔案下載紀錄 : </span>
+                            <v-spacer></v-spacer>                           
+                                <template>
+                                    <v-btn v-on="on" color="#626781" :ripple="false" @click.stop="addFile">
+                                        <v-icon left color='white' size="15">fas fa-plus</v-icon>
+                                        <span style="color:white">新增案件抽查表</span>
+                                    </v-btn>
+                                </template>
+                            </template>
+                        <template v-slot:item.modify>                          
+                            <template>
+                                <!--<v-btn dark icon v-on="on" @click.stop="downloadFile">
+                                    <v-icon color='#858585'>mdi-dots-horizontal</v-icon>
+                                    <span style="color:white">下載</span>
+                                </v-btn>-->
+                                <v-btn v-on="on" color="#736DB9" @click.stop="downloadFile" :ripple="false">
+                                    <v-icon left color='white' size="15">
+                                        mdi-arrow-down
+                                    </v-icon>
+                                    <span style="color:white">下載</span>
+                                </v-btn>
+                            </template>
+
+                        </template>
+                    </com-table>
+                </v-card>
+            </div>
+        </template>
+    </app-layout>
+</template>
+
+<style>
+    * {
+        font-family: "微軟正黑體", "Roboto", sans-serif, "Arial", "Font Awesome 5 Free", "Material Design Icons", "Material Icons", "sans-serif";
+        font-weight: 400;
+        font-size: 14px;
+    }
+
+    .app-content {
+        background-color: #F2F3F7;
+    }
+
+    .content-bar {
+        font-size: 16px;
+        /*margin-top: 80px;*/
+        margin-left: 0px;
+        padding: 0px !important;
+    }
+
+    .v-toolbar, .v-card {
+        border-radius: 0px !important;
+        box-shadow: none !important;
+    }
+
+    .v-application .elevation-1 {
+        box-shadow: none !important;
+    }
+
+
+    .v-list-item__title {
+        color: lightgray;
+        font-size: 14px;
+    }
+
+    .theme--light.v-data-table > .v-data-table__wrapper > table > thead > tr > th {
+        color: rgba(0, 0, 0, 0.6);
+        background: rgba(98, 103, 129, 0.06);
+    }
+</style>
+
+
+
+
+<script>
+    import appMenu from 'components/admin/menu';
+    import appLayout from 'components/admin/app_layout';
+    import comTable from 'components/table'
+    import { mapActions, mapGetters } from 'vuex'
+    export default {
+        // router,
+        data: () => ({
+            totalCount: 12,
+            itemsPerPage: 2,
+            totalVisible: 4,
+            showSelect: false,
+            selectType: '',
+            keyWord: '',
+            items: [],
+            headers: [
+                { text: '下載日期/時間', value: 'date', align: 'start', sortable: true, flex: 6 },
+                { text: '使用者', value: 'name', sortable: false, flex: 6 },
+                { text: '所屬單位', value: 'affiliation', sortable: false, flex: 6 },
+                { text: '報名類型', value: 'type', sortable: false, flex: 6 },
+                { text: '檔案標題', value: 'title', sortable: false, flex: 6 },
+                { text: '資料筆數', value: 'count', sortable: false, flex: 6 },
+                { text: '下載事由', value: 'download', sortable: false, flex: 6 },
+                { text: '操作', value: 'modify', sortable: false, flex: 6 },
+            ],
+        }),
+        computed: {
+            ...mapGetters('audit', ['getTypes']),
+        },
+        props: {
+
+        },
+        watch: {
+            selectType: function (value) {
+                if (!value)
+                    this.getAudit(1);
+            },
+            keyWord: function (value) {
+                if (!value)
+                    this.getAudit(1);
+            }
+        },
+        created: function () {
+            var page = 1;
+            this.getAudit(page);
+        },
+    methods: {
+        ...mapActions('audit', ['loadAudit']),
+        getAudit: function (page) {
+            var params = {
+                type: this.selectType,
+                keyWord: this.keyWord,
+                pageSize: this.itemsPerPage,
+                page: page,
+            };
+            this.loadAudit(params).then((r) => {
+                this.totalCount = r.totalCount;
+                this.items.splice(0);
+                r.datas.forEach((x) => this.items.push(x));
+            }).catch((e) => {
+                console.log(e);
+
+            });
+        },
+        changePage: function (pager) {
+            console.log(pager);
+            ///{ page: 2, pageSize: 20}
+            this.getAudit(pager.page);
+          },
+           addFile: function () {
+               console.log('addFile')
+            },
+            manualInput: function () {
+                console.log('manualInput')
+            },
+            downloadFile: function () {
+                console.log('downloadFile')
+            }
+        },
+        components: {
+            appLayout, appMenu, comTable
+        }
+    };
+</script>
+
+
