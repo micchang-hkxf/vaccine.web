f1442743118295245309f0774846cec010dc3f93
Yea <xyzyea@gmail.com>
2021-05-20 19:05:19 +0800

    場次內容

--- vaccine.web/vaccine.web/clientapps/src/components/admin/contents/registration_content.vue
+++ vaccine.web/vaccine.web/clientapps/src/components/admin/contents/registration_content.vue
@@ -15,10 +15,10 @@
                                :change-page="changePage"
                                style="margin-left: 15px;padding-top: 15px;margin-right: 15px;">
                         <!--<template v-slot:item.date="{item}">
-        <div>{{item}}</div>
-    </template>-->
-                        <template v-slot:item.quota>
-                            <div>45/<span style="color:dimgrey">60</span></div>
+            <div>{{item}}</div>
+        </template>-->
+                        <template v-slot:item.quota="{item}">
+                            <div>{{item.cntQuota}}/<span style="color:#626781">{{item.totalQuota}}</span></div>
                         </template>
                         <template v-slot:search-bar>
                             <div style="display:flex;justify-content:flex-start;margin-left:10px;margin-top:10px;">
@@ -180,7 +180,7 @@
 
                         </template>
 
-                        <template v-slot:item.modify>
+                        <template v-slot:item.modify="{item}">
                             <v-menu bottom right offset-y>
                                 <template v-slot:activator="{ on }">
                                     <v-btn dark icon v-on="on" @click.stop="">
@@ -200,6 +200,13 @@
                                             <v-btn icon dense>
                                                 <v-icon small>far fa-trash-alt</v-icon>
                                             </v-btn>刪除
+                                        </v-list-item-action-text>
+                                    </v-list-item>
+                                    <v-list-item @click.stop="detailItem(item)">
+                                        <v-list-item-action-text>
+                                            <v-btn icon dense>
+                                                <v-icon small>far fa-edit</v-icon>
+                                            </v-btn>場次內容
                                         </v-list-item-action-text>
                                     </v-list-item>
                                     <v-list-item @click.stop="editItem(item)">
@@ -227,10 +234,92 @@
                             </v-menu>
 
                         </template>
-                       
-                      
-
                     </com-table>
+                    <!---->
+                    <com-dialog ref="dialogDetail" ref-key="dialogDetail" width="100%">
+                        <template v-slot:toolbar>
+                            場次內容
+                            <v-spacer></v-spacer>
+                            <v-btn icon @click.stop="closeDetail" :ripple="false">
+                                <v-icon color="white">fas fa-times</v-icon>
+                            </v-btn>
+                        </template>
+                        <template v-slot:content>
+                            <div class="detail-title">{{detailTitle}}</div>
+                            <div class="detail-sub-title">{{detailDistrict}}－{{detailVillage}}｜{{detailType}}</div>
+                            <div class="detail-title-desc">
+                                <div>設站時間：{{detailStationTime}}</div>
+                                <div>報名開放時間：{{detailRegistrationTime}}</div>
+                                <div>報名名額：{{detailCntQuota}}/{{detailTotalQuota}}</div>
+                                <div>承辦醫院：{{detailInstitution}}（{{detailInstutionDistrict}}）</div>
+                            </div>
+                            <hr/>
+                            <!---->
+                            <com-table ref-key="detailTable" :headers="getRegistrationHeaders" :items="detailItems" :total-count="detailTotalCount"
+                                       :items-per-page="detailItemsPerPage" :total-visible="detailTotalVisible" :show-select="false"
+                                       :change-page="detailChangePage"
+                                       style="margin-left: 15px;padding-top: 15px;margin-right: 15px;">
+
+                                <template v-slot:search-bar>
+                                    <div style="display:flex;justify-content:flex-start;margin-left:10px;margin-top:10px;">
+                                        <v-text-field v-model="detailKeyWord" placeholder="請輸入報名者姓名或身分證" outlined dense clearable class="detail-search-input"></v-text-field>
+                                        <v-btn icon color="#626781" style="top:5px;" :ripple="false"
+                                               @click="getDetailForm(1)">
+                                            <v-icon>fas fa-search</v-icon>
+                                        </v-btn>
+                                        <div class="detail-rebound-info">
+                                            <div>複檢時間：2021/05/06</div>
+                                            <div>複檢通過人數：224</div>
+                                        </div>
+                                        <div class="detail-action">
+                                            <v-btn :ripple="false">
+                                                <span>再次執行複檢（0）</span>
+                                            </v-btn>
+                                            <v-btn v-on="on" color="#736DB9" @click.stop="downloadCompleteFile" :ripple="false">
+                                                <v-icon left color='white' size="15">
+                                                    mdi-arrow-down
+                                                </v-icon>
+                                                <span style="color:white">下載完整接種同意書</span>
+                                            </v-btn>
+                                            <v-btn v-on="on" color="#736DB9" @click.stop="downloadSignUpFile" :ripple="false">
+                                                <v-icon left color='white' size="15">
+                                                    mdi-arrow-down
+                                                </v-icon>
+                                                <span style="color:white">下載報名清冊</span>
+                                            </v-btn>
+                                            <v-btn v-on="on" color="#736DB9" @click.stop="downloadVaccinationFile" :ripple="false">
+                                                <v-icon left color='white' size="15">
+                                                    mdi-arrow-down
+                                                </v-icon>
+                                                <span style="color:white">下載接種清冊</span>
+                                            </v-btn>
+                                        </div>
+                                    </div>
+                                </template>
+
+                                <template v-slot:toolbar-action>
+                                    <v-spacer></v-spacer>
+                                    <div>*以上檔案可於複檢後下載</div>
+                                </template>
+
+                                <template v-slot:item.result="{item}">
+                                    <div :class="item.result === '系統異常' ? 'detail-result-abnormal' : ''">{{item.result}}</div>
+                                </template>
+
+                                <template v-slot:item.modify="{item}">
+                                    <template>
+                                        <v-btn v-on="on" color="#736DB9" @click.stop="downloadAgreeFile(item)" :ripple="false">
+                                            <v-icon left color='white' size="15">
+                                                mdi-arrow-down
+                                            </v-icon>
+                                            <span style="color:white">下載同意書</span>
+                                        </v-btn>
+                                    </template>
+                                </template>
+
+                            </com-table>
+                        </template>
+                    </com-dialog>
                 </v-card>
             </div>
         </template>
@@ -274,10 +363,59 @@
         color: rgba(0, 0, 0, 0.6);
         background: rgba(98, 103, 129, 0.06);
     }
+
+    .detail-title {
+        font: normal normal normal 24px/24px Noto Sans T Chinese;
+        letter-spacing: 0px;
+        color: #736DB9;
+        text-align: left;
+        opacity: 1;
+    }
+
+    .detail-sub-title {
+        font: normal normal normal 16px/24px Noto Sans T Chinese;
+        letter-spacing: 0px;
+        color: #626781;
+        text-align: left;
+        opacity: 1;
+        margin: 4px 0;
+    }
+
+    .detail-title-desc {
+        font: normal normal normal 16px/24px Noto Sans T Chinese;
+        letter-spacing: 0px;
+        color: #626781;
+        text-align: left;
+        opacity: 1;
+        margin: 23px 0 18px 0;
+    }
+
+    .detail-rebound-info {
+        font: normal normal normal 16px/24px Noto Sans T Chinese;
+        letter-spacing: 0px;
+        color: #626781;
+        text-align: right;
+        opacity: 1;
+        white-space: nowrap;
+        margin: -5px 30px 0 30px;
+    }
+
+    .detail-search-input {
+        width: 220px;
+    }
+
+    .detail-action .v-btn {
+        margin: 1px 5px;
+    }
+
+    .detail-result-abnormal {
+        font: normal normal normal 16px/24px Noto Sans T Chinese;
+        letter-spacing: 0px;
+        color: #F0524B;
+        text-align: center;
+        opacity: 1;
+    }
 </style>
-
-
-
 
 <script>
     import appMenu from 'components/admin/menu';
@@ -312,9 +450,25 @@
             viewerTitle:'',
             alertTitle: '',
             alertText: '',
+            detailId: '',
+            detailTitle: '',
+            detailDistrict: '',
+            detailVillage: '',
+            detailType: '',
+            detailInstitution: '',
+            detailInstutionDistrict: '',
+            detailStationTime: '',
+            detailRegistrationTime: '',
+            detailCntQuota: '',
+            detailTotalQuota: '',
+            detailItems: [],
+            detailTotalCount: 6,
+            detailItemsPerPage: 2,
+            detailTotalVisible: 4,
+            detailKeyWord: '',
         }),
         computed: {
-            ...mapGetters('registration', ['getHeaders', 'getVaccines', 'getDistricts', 'getVillages', 'getInstitutions']),
+            ...mapGetters('registration', ['getHeaders', 'getVaccines', 'getDistricts', 'getVillages', 'getInstitutions', 'getRegistrationHeaders']),
         },
         props: {
 
@@ -324,7 +478,7 @@
             this.getRegistForm(page);
         },
         methods: {
-            ...mapActions('registration', ['loadRegistForm']),
+            ...mapActions('registration', ['loadRegistForm', 'loadDetailForm']),
             getRegistForm: function (page) {
                 var params = {
                     vaccine: this.selectVaccine,
@@ -401,6 +555,57 @@
             },
             alertRightClick: function () {
                 this.$bus.$emit(`confirm_show`, false);
+            },
+            detailItem: function (item) {
+                this.detailId = item.id;
+                this.detailTitle = item.title;
+                this.detailType = item.type;
+                this.detailDistrict = item.district;
+                this.detailVillage = item.village;
+                this.detailInstitution = item.institution;
+                this.detailInstutionDistrict = item.instutionDistrict;
+                this.detailStationTime = item.stationTime;
+                this.detailRegistrationTime = item.registrationTime;
+                this.detailCntQuota = item.cntQuota;
+                this.detailTotalQuota = item.totalQuota;
+
+                this.$bus.$emit('dialogDetail_show', true);
+
+                this.getDetailForm(1);
+            },
+            closeDetail: function () {
+                this.$refs.dialogDetail.close();
+            },
+            detailChangePage: function (pager) {
+                console.log(pager);
+                this.getDetailForm(pager.page);
+            },
+            getDetailForm: function (page) {
+                var params = {
+                    id: this.detailId,
+                    keyWord: this.detailKeyWord,
+                    pageSize: this.detailItemsPerPage,
+                    page: page,
+                };
+                this.loadDetailForm(params).then((r) => {
+                    this.detailTotalCount = r.totalCount;
+                    this.detailItems.splice(0);
+                    r.datas.forEach((x) => this.detailItems.push(x));
+                }).catch((e) => {
+                    console.log(e);
+                });
+            },
+            downloadCompleteFile: function () {
+                console.log('downloadCompleteFile');
+            },
+            downloadSignUpFile: function () {
+                console.log('downloadSignUpFile');
+            },
+            downloadVaccinationFile: function () {
+                console.log('downloadSignUpFile');
+            },
+            downloadAgreeFile: function (item) {
+                console.log('downloadAgreeFile ' + item.id);
             }
         },
 
