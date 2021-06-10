840d11a697ce0732802e2704f43ce510b3ea238a
Yea <xyzyea@gmail.com>
2021-06-10 11:02:32 +0800

    套用 API (未完成)

--- vaccine.web/vaccine.web/clientapps/src/stores/registrationApiStore.js
+++ vaccine.web/vaccine.web/clientapps/src/stores/registrationApiStore.js
@@ -181,52 +181,83 @@ export default {
                 });
             });
         },
-        getCompleteFile: function ({ state }, data) {
-            return new Promise(function (resolve, reject) {
-                // TODO:
-                var result = { id: data.id, state: state };
-                try {
-                    resolve(result);
-                    alert('下載完成 (' + data.id + ')');
-                } catch (e) {
-                    reject(result);
-                }
+        getCompleteFile: function ({ state, rootGetters }, params) {
+            return new Promise((reslove, reject) => {
+                var apiUrl = `${state.apiRoot}api/Activity/Export/Agreement`;
+                var results = { datas: [], state: '' };
+
+                axios.get(apiUrl, {
+                    params: {
+                        activityId: params.id
+                    },
+                    headers: {
+                        'x-token': rootGetters['user/getToken']
+                    }
+                }).then(res => {
+                    results.datas = res.data;
+                    reslove(results);
+                }).catch(ex => {
+                    results.datas = ex;
+                    reject(results);
+                });
             });
         },
-        getSignUpFile: function ({ state }, data) {
-            return new Promise(function (resolve, reject) {
-                // TODO:
-                var result = { id: data.id, state: state };
-                try {
-                    resolve(result);
-                    alert('下載完成 (' + data.id + ')');
-                } catch (e) {
-                    reject(result);
-                }
+        getSignUpFile: function ({ state, rootGetters }, params) {
+            return new Promise((reslove, reject) => {
+                var apiUrl = `${state.apiRoot}api/Activity/Export/ApplyList`;
+                var results = { datas: [], state: '' };
+
+                axios.get(apiUrl, {
+                    params: {
+                        activityId: params.id
+                    },
+                    headers: {
+                        'x-token': rootGetters['user/getToken']
+                    }
+                }).then(res => {
+                    results.datas = res.data;
+                    reslove(results);
+                }).catch(ex => {
+                    results.datas = ex;
+                    reject(results);
+                });
             });
         },
-        getVaccinationFile: function ({ state }, data) {
-            return new Promise(function (resolve, reject) {
-                // TODO:
-                var result = { id: data.id, state: state };
-                try {
-                    resolve(result);
-                    alert('下載完成 (' + data.id + ')');
-                } catch (e) {
-                    reject(result);
-                }
+        getVaccinationFile: function ({ state, rootGetters }, params) {
+            return new Promise((reslove, reject) => {
+                var apiUrl = `${state.apiRoot}api/Activity/Export/VaccinationList`;
+                var results = { datas: [], state: '' };
+
+                axios.get(apiUrl, {
+                    params: {
+                        activityId: params.id
+                    },
+                    headers: {
+                        'x-token': rootGetters['user/getToken']
+                    }
+                }).then(res => {
+                    results.datas = res.data;
+                    reslove(results);
+                }).catch(ex => {
+                    results.datas = ex;
+                    reject(results);
+                });
             });
         },
-        getAgreeFile: function ({ state }, data) {
-            return new Promise(function (resolve, reject) {
-                // TODO:
-                var result = { id: data.id, state: state };
-                try {
-                    resolve(result);
-                    alert('下載完成 (' + data.id + ')');
-                } catch (e) {
-                    reject(result);
-                }
+        getAgreeFile: function ({ state, rootGetters }, params) {
+            return new Promise((reslove, reject) => {
+                var apiUrl = `${state.apiRoot}api/Activity/Export/Agreement/` + params.id;
+                var results = { datas: [], state: '' };
+
+                axios.get(apiUrl,
+                    rootGetters['user/getApiHeader']
+                ).then(res => {
+                    results.datas = res.data;
+                    reslove(results);
+                }).catch(ex => {
+                    results.datas = ex;
+                    reject(results);
+                });
             });
         },
         execCheck: function ({ state }, data) {
@@ -327,73 +358,6 @@ export default {
             { text: '複檢合格數', value: 'regist_qualified', sortable: false, flex: 6 },
             { text: '', value: 'modify', sortable: false },
         ],
-        desserts: [
-            {
-                regist_id:'1',
-                regist_create_date: new Date().toISOString().substr(0, 10).replace(/-/g, '/'),///'2021/04/01',
-                regist_title: '110年5月份新冠疫苗 接種',
-                regist_type: 'coronavirus',
-                regist_type_name: '新冠肺炎',
-                regist_brand: 'bnt',
-                regist_brand_name:'Pfizer-BioNTech',
-                regist_district: 'neihu',
-                regist_district_name: '內湖區',
-                regist_village: 'xikang',
-                regist_village_name: '西康里',
-                regist_place:'地點',
-                regist_institution: 'wang',
-                regist_institution_name: '王慶森診所',
-                regist_instution_district: 'neihu',
-                regist_instution_district_name: '內湖區',
-                regist_station_date: '2021/05/08',
-                regist_station_start_time: '08:30',
-                regist_station_end_time: '11:30',
-                regist_apply_start_date: new Date().toISOString().substr(0, 16).replace(/-/g, '/').replace('T', ' '),
-                regist_apply_end_date: new Date().toISOString().substr(0, 16).replace(/-/g, '/').replace('T', ' '),
-                regist_review_date: new Date().toISOString().substr(0, 10).replace(/-/g, '/'),///checkTime              
-                regist_qualified: 423,
-                regist_quota: 500,
-                regist_unpassed: 45,
-                //abnormalCnt: '0',
-               
-                //checkPassCnt: '0',
-                //cntQuota: '425',
-                //totalQuota: '500',
-                //qualified: '423',
-                //abnormalCnt: '0',
-                //checkTime: '2021/05/20',
-                //checkPassCnt: '224',
-            },
-            {
-                regist_id: '2',
-                regist_create_date: new Date().toISOString().substr(0, 10).replace(/-/g, '/'),///'2021/04/01',
-                regist_title: '110年10月份新冠疫苗 接種',
-                regist_type: 'coronavirus',
-                regist_type_name: '新冠肺炎',
-                regist_brand: 'bnt',
-                regist_brand_name: 'Pfizer-BioNTech',
-                regist_district: 'neihu',
-                regist_district_name: '內湖區',
-                regist_village: 'xikang',
-                regist_village_name: '西康里',
-                regist_place: '設站地點',
-                regist_institution: 'wang',
-                regist_institution_name: '王慶森診所',
-                regist_instution_district: 'neihu',
-                regist_instution_district_name: '內湖區',
-                regist_station_date: new Date().toISOString().substr(0, 10).replace(/-/g, '/'),//'2021/05/08',
-                regist_station_start_time: '08:30',
-                regist_station_end_time: '11:30',
-                regist_apply_start_date: new Date().toISOString().substr(0, 10).replace(/-/g, '/').replace('T', ' '),
-                regist_apply_end_date: new Date().toISOString().substr(0, 10).replace(/-/g, '/').replace('T', ' '),
-                regist_review_date: new Date().toISOString().substr(0, 10).replace(/-/g, '/'),///checkTime              
-                regist_qualified: '423',
-                regist_quota: 500,
-                regist_unpassed: 45,
-
-                //checkPassCnt: '0',
-            },
-        ],
         brands: [
             { id: 'az', name: 'AstraZeneca' },
             { id: 'bnt', name: 'Pfizer-BioNTech' },
@@ -401,12 +365,10 @@ export default {
         vaccines: [
             { id: '0', name: '肺鏈流感' },
             { id: '1', name: '新冠肺炎' },
-
         ],
         districts: [
             { id: 'neihu', name: '內湖區' },
             { id: 'nangang', name: '南港區' },
-
         ],
         villages: [
             { id: 'xikang', name: '西康里' }
@@ -429,106 +391,10 @@ export default {
             { text: '複檢結果', value: 'result', sortable: false, flex: 6 },
             { text: '', value: 'modify', sortable: false },
         ],
-        registrationDesserts: [
-            {
-                id: '452',
-                date: '2021/05/08 09:59',
-                name: '袁吉吉',
-                serialNumber: '452',
-                gender: 'M',
-                birthday: '1983/11/08',
-                identity: 'B128541236',
-                phone: '0910123456',
-                censusRegister: '北市',
-                type: '現場',
-                result: '',
-            },
-            {
-                id: '451',
-                date: '2021/05/08 09:50',
-                name: '柯安安',
-                serialNumber: '451',
-                gender: 'F',
-                birthday: '1970/05/23',
-                identity: 'B125463987',
-                phone: '0910123434',
-                censusRegister: '非北市',
-                type: '現場',
-                result: '合格',
-            },
-            {
-                id: '450',
-                date: '2021/04/28 23:32',
-                name: '張蘭蘭',
-                serialNumber: '450',
-                gender: 'F',
-                birthday: '1995/06/18',
-                identity: 'F125463990',
-                phone: '0910123490',
-                censusRegister: '非北市',
-                type: '事先里辦',
-                result: '不合格',
-            },
-            {
-                id: '449',
-                date: '2021/04/18 01:31',
-                name: '李良良',
-                serialNumber: '449',
-                gender: 'F',
-                birthday: '1995/06/18',
-                identity: 'F125463990',
-                phone: '0910123490',
-                censusRegister: '北市',
-                type: '事先網路',
-                result: '已取消',
-            },
-            {
-                id: '448',
-                date: '2021/04/15 18:15',
-                name: '趙強強',
-                serialNumber: '448',
-                gender: 'M',
-                birthday: '1973/05/05',
-                identity: 'A125463939',
-                phone: '0910123455',
-                censusRegister: '北市',
-                type: '事先里辦',
-                result: '合格',
-            },
-            {
-                id: '447',
-                date: '2021/04/11 11:15',
-                name: '謝昌昌',
-                serialNumber: '447',
-                gender: 'M',
-                birthday: '1973/05/15',
-                identity: 'A125463949',
-                phone: '0910123457',
-                censusRegister: '北市',
-                type: '事先里辦',
-                result: '系統異常',
-            },
-            {
-                id: '446',
-                date: '2021/04/11 11:15',
-                name: '阿吉吉',
-                serialNumber: '446',
-                gender: 'F',
-                birthday: '1978/01/12',
-                identity: 'A125463882',
-                phone: '0910123462',
-                censusRegister: '北市',
-                type: '事先里辦',
-                result: '不合格(人工複檢)',
-            },
-        ],
     },
     getters: {
         getHeaders: state => {
             return state.headers;
-        },
-        getDesserts: state => {
-            return state.desserts;
         },
         getVaccines: state => {
             return state.vaccines;
@@ -544,9 +410,6 @@ export default {
         },
         getRegistrationHeaders: state => {
             return state.registrationHeaders;
-        },
-        getRegistrationDesserts: state => {
-            return state.registrationDesserts;
         },
     },
     mutations: {
