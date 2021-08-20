73c85e3810eeeb0495313cc9b85371467bc8eca0
huwyang <huwyang@macisk.idv.tw>
2021-08-20 18:14:06 +0800

    commit for 報名頁面版面調整

--- vaccine.web/vaccine.web/clientapps/src/components/regist/contents/agree_content.vue
+++ vaccine.web/vaccine.web/clientapps/src/components/regist/contents/agree_content.vue
@@ -95,7 +95,10 @@
 </script>
 <style scoped>
     .agree-content/deep/ .app-content {
-        margin-bottom: 78px;
+        /*margin-bottom: 78px;*/
+        width:65%;
+        margin: 0 auto;
+        /*padding: 32px 16px 87px;*/
     }
 
     .agree-content/deep/ .action-header {
