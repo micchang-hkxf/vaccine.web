f70b428f470b05318dc13ee1f7816d591f702f69
LeoHuang <tfx88@hotmail.com>
2021-09-09 10:02:00 +0800

    場次內容-自次複檢次數顯示修正

--- vaccine.web/vaccine.web/clientapps/src/components/admin/contents/registration_content.vue
+++ vaccine.web/vaccine.web/clientapps/src/components/admin/contents/registration_content.vue
@@ -1654,6 +1654,7 @@
                 var comp = this;
                 this.detailAbnormalCnt = 0;
                 this.loadDetailForm(params).then((r) => {
+
                     comp.detailTotalCount = r.totalCount;
                     comp.activityId = r.activityId;
                     comp.reCheckId = r.reCheckId;
