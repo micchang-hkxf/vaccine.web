c3d4064b03519778fd333f1df88cdba697e1bae7
micchang <micchang@g.hkxf.org>
2021-09-28 17:18:06 +0800

    commit for 檔案檢查

--- vaccine.web/vaccine.web/clientapps/src/stores/registeredApiStore.js
+++ vaccine.web/vaccine.web/clientapps/src/stores/registeredApiStore.js
@@ -29,7 +29,7 @@ export default {
                             identity: data.uId,
                             vaccination: data.activityName,
                             way: data.applyText,
-                            result: (data.eligible) ? "合格" : "不合格",
+                            result: (data.logType==1) ? "合格" : "不合格",
                             number: data.applyNo,
                             distName: data.region.distName
                         });
